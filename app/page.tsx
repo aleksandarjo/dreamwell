1c6cf502059312ada0d87d55179de5bd1a6ab49f
Aleksandarjo <aleksandar.joovanoovic@gmail.com>
2025-01-25 20:02:59 +0100

    feat: implement property card component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+import PropertyCard from "@/components/cards/property-card";
+
+// todo configure eslint and prettier to sort the imports, tailwind classes and remove the unused imports
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="min-h-screen bg-white  py-10 px-2.5">
+      <PropertyCard />
+      <PropertyCard orientation="horizontal" />
+    </div>
+  );
 }
--- constants/index.tsx
+++ constants/index.tsx
@@ -1,5 +1,7 @@
 import { ROUTES } from "./route";
 
+import { bathIcon, bedIcon, garageIcon, squareIcon } from "@/public/index";
+
 export const navLinks = [
   {
     route: ROUTES.HOME,
@@ -21,4 +23,35 @@ export const navLinks = [
     route: ROUTES.BLOG,
     label: "Blog",
   },
-];
+] as const;
+
+export const propertyAttributes = [
+  {
+    id: 1,
+    label: "2 beds",
+    src: bedIcon,
+    alt: "bed",
+    isEven: false,
+  },
+  {
+    id: 2,
+    label: "no garage",
+    src: garageIcon,
+    alt: "garage",
+    isEven: true,
+  },
+  {
+    id: 3,
+    label: "2 baths",
+    src: bathIcon,
+    alt: "bath",
+    isEven: false,
+  },
+  {
+    id: 4,
+    label: "1,235 sqft",
+    src: squareIcon,
+    alt: "square",
+    isEven: true,
+  },
+] as const;
